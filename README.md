2f07a9155fdfac20e68e38ed27a2aa4e1493a1d8
Sven Dowideit <SvenDowideit@docker.com>
2014-08-05 23:13:54 +1000

    it works on my OSX box, using b2d

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ generate_cert
 =============
 
 generate the tls certs needed for Docker TLS socket.
+
+run:
+
+- `./make.sh`
+
+Which will generate the certificate files you need and put them into your `~/.docker` dir
+
+Then copy the certs from `~/.docker/` to your boot2docker and start the server with them:
+
+- `sudo docker -d --tlsverify --tlscacert=ca.pem --tlscert=servercert.pem --tlskey=serverkey.pem -H tcp://0.0.0.0:2376 -D`
+
+then back on your OSX box, you can run:
+
+- docker -H 192.168.59.103:2376 --tls version
+
+
---
+++ make.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+go build generate_cert.go
+
+mkdir -p ${HOME}/.docker/boot2docker
+cp generate_cert ${HOME}/.docker/
+
+# TODO: https://github.com/docker/docker/issues/7418
+# When I use DOCKER_CONFIG tls doesn't seem to work as documented
+#cd ${HOME}/.docker/boot2docker
+cd ${HOME}/.docker
+
+./generate_cert --cert=ca.pem --key=cakey.pem
+./generate_cert --host=boot2docker,192.168.59.103 --ca=ca.pem --ca-key=cakey.pem --cert=servercert.pem --key=serverkey.pem
+./generate_cert --ca=ca.pem --ca-key=cakey.pem --cert=cert.pem --key=key.pem
+
+#echo "to use the 'boot2docker' tls certificates, set:"
+#echo "    export DOCKER_CONFIG=${HOME}/.docker/boot2docker"
